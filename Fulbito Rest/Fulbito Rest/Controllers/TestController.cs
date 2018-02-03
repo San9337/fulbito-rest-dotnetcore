d6752a27e4c3d01a4e63f45498be77ae6d9f82b1
Santiago Gomez Cerruti <san93gc@gmail.com>
2018-02-03 10:28:47 -0300

    Project creation + Test controllers (api/test/hello and api/test/param?param)

---
+++ Fulbito Rest/Fulbito Rest/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fulbito_Rest.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("Hello")]
+        public string Hello()
+        {
+            //http://localhost:65520/api/test/hello
+            return "Hi Eze";
+        }
+
+        [HttpGet]
+        [Route("Param")]
+        public string Param(string param)
+        {
+            //http://localhost:65520/api/test/param?param="hi"
+            return param;
+        }
+    }
+}
