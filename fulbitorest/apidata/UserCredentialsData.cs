4d1f5af1da3aeaa76a4ff77fb0da8a4a89f70577
Santiago Gomez Cerruti <san93gc@gmail.com>
2018-02-12 11:05:28 -0300

    Debug post problem: datamembers

--- fulbitorest/apidata/UserCredentialsData.cs
+++ fulbitorest/apidata/UserCredentialsData.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace apidata
 {
+    [DataContract]
     public class UserCredentialsData
     {
+        [DataMember(Name ="user")]
         public string User { get; set; }
+
+        [DataMember(Name ="password")]
         public string Password { get; set; }
     }
 }
